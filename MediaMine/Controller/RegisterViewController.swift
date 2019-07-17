bf132529c4851e4bed3149c61c4e1bfdd913054f
Christopher Chu <christopherinsup@gmail.com>
2019-07-17 13:41:24 -0700

    add login ui

---
+++ MediaMine/Controller/RegisterViewController.swift
@@ -0,0 +1,47 @@
+//
+//  RegisterViewController.swift
+//  MediaMine
+//
+//  Created by Christopher Chu on 7/16/19.
+//  Copyright © 2019 Christopher Chu. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    
+    @IBOutlet weak var userName: UITextField!
+    @IBOutlet weak var userPassword: UITextField!
+    @IBOutlet weak var verifyPassword: UITextField!
+    @IBOutlet weak var userEmail: UITextField!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+
+}
