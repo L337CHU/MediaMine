e47057f42e49f479acdb2f68c806cb7bf1b6dc9d
Amy <amylstockinger@gmail.com>
2019-07-26 10:01:55 -0700

    update UI

---
+++ MediaMine/Controller/PostViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PostViewController.swift
+//  MediaMine
+//
+//  Created by Amy Stockinger on 7/26/19.
+//  Copyright © 2019 Amy Stockinger. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PostViewController: UIViewController {
+    
+}
+// will likely have to pass postID in segue from company view to this view
